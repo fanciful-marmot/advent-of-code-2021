efd2d90917d5f5d462476bcf36657834e837a9db
Daniel <daniel.crookston@gmail.com>
2021-12-03 13:40:03 -0500

    Day 3 part 2 in rust. Dear god, lifetimes, why

--- day-03-binary-diagnostic/rust/src/main.rs
+++ day-03-binary-diagnostic/rust/src/main.rs
@@ -47,6 +47,60 @@ fn part1(lines: &Vec<String>) -> u32 {
     gamma * epsilon
 }
 
-fn part2(_lines: &Vec<String>) -> u32 {
-    0
+fn split_filter_at_bit<'a>(lines: &Vec<&'a String>, bit_index: usize) -> (Vec<&'a String>, Vec<&'a String>) {
+    let mut zero = Vec::new();
+    let mut one = Vec::new();
+
+    for &line in lines {
+        if line.chars().nth(bit_index).unwrap() == '0' {
+            zero.push(line);
+        } else {
+            one.push(line);
+        }
+    }
+
+    (zero, one)
+}
+
+fn part2(lines: &Vec<String>) -> u32 {
+    let num_digits = lines[0].len();
+
+    // Determine majority
+    let oxygen_rating = {
+        let mut majority: Vec<&String> = lines.iter().collect();
+        for i in 0..num_digits {
+            let (zero, one) = split_filter_at_bit(&majority, i);
+            majority = if one.len() >= zero.len() {
+                one
+            } else {
+                zero
+            };
+
+            if majority.len() == 1 {
+                break;
+            }
+        }
+        isize::from_str_radix(&majority[0], 2).unwrap() as u32
+    };
+
+    // Determine majority
+    let co2_scrubber = {
+        let mut minority: Vec<&String> = lines.iter().collect();
+        for i in 0..num_digits {
+            let (zero, one) = split_filter_at_bit(&minority, i);
+            minority = if one.len() >= zero.len() {
+                zero
+            } else {
+                one
+            };
+
+            if minority.len() == 1 {
+                break;
+            }
+        }
+        isize::from_str_radix(&minority[0], 2).unwrap() as u32
+    };
+
+
+    oxygen_rating * co2_scrubber
 }
